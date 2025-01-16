04a3079566889cde69bbd565059b81363d2b71a9
vishwa satasiya <vishwa.s@sarvadhi.com>
2025-01-16 10:39:15 +0530

    Add "Join & Leave Room" functionality

--- index.js
+++ index.js
@@ -9,10 +9,26 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("A new user has connected", socket.id);
 
-  socket.on("chat-message", (message) => {
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
+    socket
+      .to(room)
+      .emit("new-notification", `User ${socket.id} has joined the room.`);
+  });
+
+  socket.on("leave-room", (room) => {
+    socket.leave(room);
+    console.log(`User ${socket.id} left room: ${room}`);
+    socket
+      .to(room)
+      .emit("new-notification", `User ${socket.id} has left the room.`);
+  });
+
+  socket.on("chat-message", (message, room) => {
     socket.emit("add-message", message, "self");
-    socket.broadcast.emit("add-message", message, "other");
-    socket.broadcast.emit("new-notification", `User ${socket.id}: ${message}`);
+    socket.to(room).emit("add-message", message, "other");
+    socket.to(room).emit("new-notification", `User ${socket.id}: ${message}`);
   });
 });
 
